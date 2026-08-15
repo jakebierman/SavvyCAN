56dcb0d7994e8e2e78f92ceb0badad41273328b7
Jake Bierman <your-email@example.com>
2026-08-15 22:46:07 +0800

    V229 auto release test

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 #ifndef CONFIG
 #define CONFIG
 
-#define VERSION             228
+#define VERSION             229
 
 //try to keep this in sync.
 //SavvyCAN will complain if you connect a GVRET board with a revision
